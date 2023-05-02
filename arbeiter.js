4f31780ed41f7f71d8d646449afc4b8130988054
AndySchw <andy.emich@docc.techstarter.de>
2023-05-02 17:16:30 +0200

    so ziemlich fertig

--- arbeiter.js
+++ arbeiter.js
@@ -157,13 +157,17 @@ var preis4 = 280
 function anzahl4erh(){
     count4 = count4 + 1 
     countEl4.innerText = ("Anzahl: ") + count4
+    addToCart({ name: document.getElementById(`4-product-title`).innerText, price: preis4, quantity: count4})
     komplettpreis()
+    updateCartDisplay()
 }
 
 function anzahl4senk(){
     if(count4>=1){count4=count4-1
     countEl4.innerText = ("Anzahl: ") + count4}
-    komplettpreis()
+    removeFromCart({ name: document.getElementById(`4-product-title`).innerText});
+    komplettpreis();
+    updateCartDisplay();
     
 };
 
@@ -187,13 +191,17 @@ var preis5 = 499
 function anzahl5erh(){
     count5 = count5 + 1 
     countEl5.innerText = ("Anzahl: ") + count5
+    addToCart({ name: document.getElementById(`5-product-title`).innerText, price: preis5, quantity: count5})
     komplettpreis()
+    updateCartDisplay()
 }
 
 function anzahl5senk(){
     if(count5>=1){count5=count5-1
     countEl5.innerText = ("Anzahl: ") + count5}
-    komplettpreis()
+    removeFromCart({ name: document.getElementById(`5-product-title`).innerText});
+    komplettpreis();
+    updateCartDisplay();
     
 };
 
@@ -217,13 +225,17 @@ var preis6 = 1749
 function anzahl6erh(){
     count6 = count6 + 1 
     countEl6.innerText = ("Anzahl: ") + count6
+    addToCart({ name: document.getElementById(`6-product-title`).innerText, price: preis6, quantity: count6})
     komplettpreis()
+    updateCartDisplay()
 }
 
 function anzahl6senk(){
     if(count6>=1){count6=count6-1
     countEl6.innerText = ("Anzahl: ") + count6}
-    komplettpreis()
+    removeFromCart({ name: document.getElementById(`6-product-title`).innerText});
+    komplettpreis();
+    updateCartDisplay();
     
 };
 
@@ -247,13 +259,17 @@ var preis7 = 1499
 function anzahl7erh(){
     count7 = count7 + 1 
     countEl7.innerText = ("Anzahl: ") + count7
+    addToCart({ name: document.getElementById(`7-product-title`).innerText, price: preis7, quantity: count7})
     komplettpreis()
+    updateCartDisplay()
 }
 
 function anzahl7senk(){
     if(count7>=1){count7=count7-1
     countEl7.innerText = ("Anzahl: ") + count7}
-    komplettpreis()
+    removeFromCart({ name: document.getElementById(`7-product-title`).innerText});
+    komplettpreis();
+    updateCartDisplay();
     
 };
 
@@ -277,13 +293,17 @@ var preis8 = 1499
 function anzahl8erh(){
     count8 = count8 + 1 
     countEl8.innerText = ("Anzahl: ") + count8
+    addToCart({ name: document.getElementById(`8-product-title`).innerText, price: preis8, quantity: count8})
     komplettpreis()
+    updateCartDisplay()
 }
 
 function anzahl8senk(){
     if(count8>=1){count8=count8-1
     countEl8.innerText = ("Anzahl: ") + count8}
-    komplettpreis()
+    removeFromCart({ name: document.getElementById(`8-product-title`).innerText});
+    komplettpreis();
+    updateCartDisplay();
     
 };
 
@@ -307,13 +327,17 @@ var preis9 = 1099
 function anzahl9erh(){
     count9 = count9 + 1 
     countEl9.innerText = ("Anzahl: ") + count9
+    addToCart({ name: document.getElementById(`9-product-title`).innerText, price: preis9, quantity: count9})
     komplettpreis()
+    updateCartDisplay()
 }
 
 function anzahl9senk(){
     if(count9>=1){count9=count9-1
     countEl9.innerText = ("Anzahl: ") + count9}
-    komplettpreis()
+    removeFromCart({ name: document.getElementById(`9-product-title`).innerText});
+    komplettpreis();
+    updateCartDisplay();
     
 };
 
@@ -337,13 +361,17 @@ var preis10 = 1099
 function anzahl10erh(){
     count10 = count10 + 1 
     countEl10.innerText = ("Anzahl: ") + count10
+    addToCart({ name: document.getElementById(`10-product-title`).innerText, price: preis10, quantity: count10})
     komplettpreis()
+    updateCartDisplay()
 }
 
 function anzahl10senk(){
     if(count10>=1){count10=count10-1
     countEl10.innerText = ("Anzahl: ") + count10}
-    komplettpreis()
+    removeFromCart({ name: document.getElementById(`10-product-title`).innerText});
+    komplettpreis();
+    updateCartDisplay();
     
 };
 
@@ -367,13 +395,17 @@ var preis11 = 13
 function anzahl11erh(){
     count11 = count11 + 1 
     countEl11.innerText = ("Anzahl: ") + count11
+    addToCart({ name: document.getElementById(`11-product-title`).innerText, price: preis11, quantity: count11})
     komplettpreis()
+    updateCartDisplay()
 }
 
 function anzahl11senk(){
     if(count11>=1){count11=count11-1
     countEl11.innerText = ("Anzahl: ") + count11}
-    komplettpreis()
+    removeFromCart({ name: document.getElementById(`11-product-title`).innerText});
+    komplettpreis();
+    updateCartDisplay();
     
 };
 
@@ -397,13 +429,17 @@ var preis12 = 40
 function anzahl12erh(){
     count12 = count12 + 1 
     countEl12.innerText = ("Anzahl: ") + count12
+    addToCart({ name: document.getElementById(`12-product-title`).innerText, price: preis12, quantity: count12})
     komplettpreis()
+    updateCartDisplay()
 }
 
 function anzahl12senk(){
     if(count12>=1){count12=count12-1
     countEl12.innerText = ("Anzahl: ") + count12}
-    komplettpreis()
+    removeFromCart({ name: document.getElementById(`12-product-title`).innerText});
+    komplettpreis();
+    updateCartDisplay();
     
 };
 
@@ -427,13 +463,17 @@ var preis13 = 13
 function anzahl13erh(){
     count13 = count13 + 1 
     countEl13.innerText = ("Anzahl: ") + count13
+    addToCart({ name: document.getElementById(`13-product-title`).innerText, price: preis13, quantity: count13})
     komplettpreis()
+    updateCartDisplay()
 }
 
 function anzahl13senk(){
     if(count13>=1){count13=count13-1
     countEl13.innerText = ("Anzahl: ") + count13}
-    komplettpreis()
+    removeFromCart({ name: document.getElementById(`13-product-title`).innerText});
+    komplettpreis();
+    updateCartDisplay();
     
 };
 
@@ -457,13 +497,17 @@ var preis14 = 120
 function anzahl14erh(){
     count14 = count14 + 1 
     countEl14.innerText = ("Anzahl: ") + count14
+    addToCart({ name: document.getElementById(`14-product-title`).innerText, price: preis14, quantity: count14})
     komplettpreis()
+    updateCartDisplay()
 }
 
 function anzahl14senk(){
     if(count14>=1){count14=count14-1
     countEl14.innerText = ("Anzahl: ") + count14}
-    komplettpreis()
+    removeFromCart({ name: document.getElementById(`14-product-title`).innerText});
+    komplettpreis();
+    updateCartDisplay();
     
 };
 
@@ -487,13 +531,17 @@ var preis15 = 30
 function anzahl15erh(){
     count15 = count15 + 1 
     countEl15.innerText = ("Anzahl: ") + count15
+    addToCart({ name: document.getElementById(`15-product-title`).innerText, price: preis15, quantity: count15})
     komplettpreis()
+    updateCartDisplay()
 }
 
 function anzahl15senk(){
     if(count15>=1){count15=count15-1
     countEl15.innerText = ("Anzahl: ") + count15}
-    komplettpreis()
+    removeFromCart({ name: document.getElementById(`15-product-title`).innerText});
+    komplettpreis();
+    updateCartDisplay();
     
 };
 
@@ -517,13 +565,17 @@ var preis16 = 19
 function anzahl16erh(){
     count16 = count16 + 1 
     countEl16.innerText = ("Anzahl: ") + count16
+    addToCart({ name: document.getElementById(`16-product-title`).innerText, price: preis16, quantity: count16})
     komplettpreis()
+    updateCartDisplay()
 }
 
 function anzahl16senk(){
     if(count16>=1){count16=count16-1
     countEl16.innerText = ("Anzahl: ") + count16}
-    komplettpreis()
+    removeFromCart({ name: document.getElementById(`16-product-title`).innerText});
+    komplettpreis();
+    updateCartDisplay();
     
 };
 
@@ -547,13 +599,17 @@ var preis17 = 12
 function anzahl17erh(){
     count17 = count17 + 1 
     countEl17.innerText = ("Anzahl: ") + count17
+    addToCart({ name: document.getElementById(`17-product-title`).innerText, price: preis17, quantity: count17})
     komplettpreis()
+    updateCartDisplay()
 }
 
 function anzahl17senk(){
     if(count17>=1){count17=count17-1
     countEl17.innerText = ("Anzahl: ") + count17}
-    komplettpreis()
+    removeFromCart({ name: document.getElementById(`17-product-title`).innerText});
+    komplettpreis();
+    updateCartDisplay();
     
 };
 
@@ -577,13 +633,17 @@ var preis18 = 40
 function anzahl18erh(){
     count18 = count18 + 1 
     countEl18.innerText = ("Anzahl: ") + count18
+    addToCart({ name: document.getElementById(`18-product-title`).innerText, price: preis18, quantity: count18})
     komplettpreis()
+    updateCartDisplay()
 }
 
 function anzahl18senk(){
     if(count18>=1){count18=count18-1
     countEl18.innerText = ("Anzahl: ") + count18}
-    komplettpreis()
+    removeFromCart({ name: document.getElementById(`18-product-title`).innerText});
+    komplettpreis();
+    updateCartDisplay();
     
 };
 
@@ -607,13 +667,17 @@ var preis19 = 46
 function anzahl19erh(){
     count19 = count19 + 1 
     countEl19.innerText = ("Anzahl: ") + count19
+    addToCart({ name: document.getElementById(`19-product-title`).innerText, price: preis19, quantity: count19})
     komplettpreis()
+    updateCartDisplay()
 }
 
 function anzahl19senk(){
     if(count19>=1){count19=count19-1
     countEl19.innerText = ("Anzahl: ") + count19}
-    komplettpreis()
+    removeFromCart({ name: document.getElementById(`19-product-title`).innerText});
+    komplettpreis();
+    updateCartDisplay();
     
 };
 
@@ -637,13 +701,17 @@ var preis20 = 70
 function anzahl20erh(){
     count20 = count20 + 1 
     countEl20.innerText = ("Anzahl: ") + count20
+    addToCart({ name: document.getElementById(`20-product-title`).innerText, price: preis20, quantity: count20})
     komplettpreis()
+    updateCartDisplay()
 }
 
 function anzahl20senk(){
     if(count20>=1){count20=count20-1
     countEl20.innerText = ("Anzahl: ") + count20}
-    komplettpreis()
+    removeFromCart({ name: document.getElementById(`20-product-title`).innerText});
+    komplettpreis();
+    updateCartDisplay();
     
 };
 
@@ -668,13 +736,17 @@ var preis21 = 70
 function anzahl21erh(){
     count21 = count21 + 1 
     countEl21.innerText = ("Anzahl: ") + count21
+    addToCart({ name: document.getElementById(`21-product-title`).innerText, price: preis21, quantity: count21})
     komplettpreis()
+    updateCartDisplay()
 }
 
 function anzahl21senk(){
     if(count21>=1){count21=count21-1
     countEl21.innerText = ("Anzahl: ") + count21}
-    komplettpreis()
+    removeFromCart({ name: document.getElementById(`21-product-title`).innerText});
+    komplettpreis();
+    updateCartDisplay();
     
 };
 
@@ -698,13 +770,17 @@ var preis22 = 14
 function anzahl22erh(){
     count22 = count22 + 1 
     countEl22.innerText = ("Anzahl: ") + count22
+    addToCart({ name: document.getElementById(`22-product-title`).innerText, price: preis22, quantity: count22})
     komplettpreis()
+    updateCartDisplay()
 }
 
 function anzahl22senk(){
     if(count22>=1){count22=count22-1
     countEl22.innerText = ("Anzahl: ") + count22}
-    komplettpreis()
+    removeFromCart({ name: document.getElementById(`22-product-title`).innerText});
+    komplettpreis();
+    updateCartDisplay();
     
 };
 
@@ -728,13 +804,17 @@ var preis23 = 14
 function anzahl23erh(){
     count23 = count23 + 1 
     countEl23.innerText = ("Anzahl: ") + count23
+    addToCart({ name: document.getElementById(`23-product-title`).innerText, price: preis23, quantity: count23})
     komplettpreis()
+    updateCartDisplay()
 }
 
 function anzahl23senk(){
     if(count23>=1){count23=count23-1
     countEl23.innerText = ("Anzahl: ") + count23}
-    komplettpreis()
+    removeFromCart({ name: document.getElementById(`23-product-title`).innerText});
+    komplettpreis();
+    updateCartDisplay();
     
 };
 
@@ -758,13 +838,17 @@ var preis24 = 46
 function anzahl24erh(){
     count24 = count24 + 1 
     countEl24.innerText = ("Anzahl: ") + count24
+    addToCart({ name: document.getElementById(`24-product-title`).innerText, price: preis24, quantity: count24})
     komplettpreis()
+    updateCartDisplay()
 }
 
 function anzahl24senk(){
     if(count24>=1){count24=count24-1
     countEl24.innerText = ("Anzahl: ") + count24}
-    komplettpreis()
+    removeFromCart({ name: document.getElementById(`24-product-title`).innerText});
+    komplettpreis();
+    updateCartDisplay();
     
 };
 
@@ -788,13 +872,17 @@ var preis25 = 70
 function anzahl25erh(){
     count25 = count25 + 1 
     countEl25.innerText = ("Anzahl: ") + count25
+    addToCart({ name: document.getElementById(`25-product-title`).innerText, price: preis25, quantity: count25})
     komplettpreis()
+    updateCartDisplay()
 }
 
 function anzahl25senk(){
     if(count25>=1){count25=count25-1
     countEl25.innerText = ("Anzahl: ") + count25}
-    komplettpreis()
+    removeFromCart({ name: document.getElementById(`25-product-title`).innerText});
+    komplettpreis();
+    updateCartDisplay();
     
 };
 
@@ -818,13 +906,17 @@ var preis26 = 41
 function anzahl26erh(){
     count26 = count26 + 1 
     countEl26.innerText = ("Anzahl: ") + count26
+    addToCart({ name: document.getElementById(`26-product-title`).innerText, price: preis26, quantity: count26})
     komplettpreis()
+    updateCartDisplay()
 }
 
 function anzahl26senk(){
     if(count26>=1){count26=count26-1
     countEl26.innerText = ("Anzahl: ") + count26}
-    komplettpreis()
+    removeFromCart({ name: document.getElementById(`26-product-title`).innerText});
+    komplettpreis();
+    updateCartDisplay();
     
 };
 
@@ -848,13 +940,17 @@ var preis27 = 41
 function anzahl27erh(){
     count27 = count27 + 1 
     countEl27.innerText = ("Anzahl: ") + count27
+    addToCart({ name: document.getElementById(`27-product-title`).innerText, price: preis27, quantity: count27})
     komplettpreis()
+    updateCartDisplay()
 }
 
 function anzahl27senk(){
     if(count27>=1){count27=count27-1
     countEl27.innerText = ("Anzahl: ") + count27}
-    komplettpreis()
+    removeFromCart({ name: document.getElementById(`27-product-title`).innerText});
+    komplettpreis();
+    updateCartDisplay();
     
 };
 
@@ -878,13 +974,17 @@ var preis28 = 20
 function anzahl28erh(){
     count28 = count28 + 1 
     countEl28.innerText = ("Anzahl: ") + count28
+    addToCart({ name: document.getElementById(`28-product-title`).innerText, price: preis28, quantity: count28})
     komplettpreis()
+    updateCartDisplay()
 }
 
 function anzahl28senk(){
     if(count28>=1){count28=count28-1
     countEl28.innerText = ("Anzahl: ") + count28}
-    komplettpreis()
+    removeFromCart({ name: document.getElementById(`28-product-title`).innerText});
+    komplettpreis();
+    updateCartDisplay();
     
 };
 
@@ -908,13 +1008,17 @@ var preis31 = 40
 function anzahl31erh(){
     count31 = count31 + 1 
     countEl31.innerText = ("Anzahl: ") + count31
+    addToCart({ name: document.getElementById(`31-product-title`).innerText, price: preis31, quantity: count31})
     komplettpreis()
+    updateCartDisplay()
 }
 
 function anzahl31senk(){
     if(count31>=1){count31=count31-1
     countEl31.innerText = ("Anzahl: ") + count31}
-    komplettpreis()
+    removeFromCart({ name: document.getElementById(`31-product-title`).innerText});
+    komplettpreis();
+    updateCartDisplay();
     
 };
 
@@ -938,13 +1042,17 @@ var preis30 = 30
 function anzahl30erh(){
     count30 = count30 + 1 
     countEl30.innerText = ("Anzahl: ") + count30
+    addToCart({ name: document.getElementById(`30-product-title`).innerText, price: preis30, quantity: count30})
     komplettpreis()
+    updateCartDisplay()
 }
 
 function anzahl30senk(){
     if(count30>=1){count30=count30-1
     countEl30.innerText = ("Anzahl: ") + count30}
-    komplettpreis()
+    removeFromCart({ name: document.getElementById(`30-product-title`).innerText});
+    komplettpreis();
+    updateCartDisplay();
     
 };
 
@@ -990,21 +1098,15 @@ function addToCart(item) {
 function removeFromCart(itemName) {
   const itemIndex = cartItems.findIndex(cartItem => cartItem.name === itemName);
   if (itemIndex !== -1) {
-    // Entfernen Sie den Artikel aus dem Warenkorb
-    cartItems.splice(itemIndex, 1);
+    const itemPrice = cartItems[itemIndex].price; // Holen Sie sich den Preis des Artikels
+    const itemQuantity = cartItems[itemIndex].quantity; // Holen Sie sich die Anzahl des Artikels
+    cartItems.splice(itemIndex, 1); // Entfernen Sie den Artikel aus dem Warenkorb
+    cartTotal -= itemPrice * itemQuantity; // Ziehen Sie den Preis des entfernten Artikels von der Gesamtsumme ab
   }
-  updateCartDisplay()
+  updateCartDisplay();
+  
 }
-// function removeFromCart(itemName) {
-//   const itemIndex = cartItems.findIndex(cartItem => cartItem.name === itemName);
-//   if (itemIndex !== -1) {
-//     const itemPrice = cartItems[itemIndex].price; // Holen Sie sich den Preis des Artikels
-//     const itemQuantity = cartItems[itemIndex].quantity; // Holen Sie sich die Anzahl des Artikels
-//     cartItems.splice(itemIndex, 1); // Entfernen Sie den Artikel aus dem Warenkorb
-//     cartTotal -= itemPrice * itemQuantity; // Ziehen Sie den Preis des
-//   }
-//   updateCartDisplay();
-//   } 
+
 
 
 
@@ -1020,17 +1122,20 @@ function updateCartDisplay() {
     
     removeButton.classList.add("btn-close");
     removeButton.addEventListener("click", () => {
-      removeFromCart(item.name);
-      updateCartDisplay(); // aktualisiert den Warenkorb nach Entfernung
-      // if (itemIndex !== -1) {
-      //   const removedItem = cartItems.splice(itemIndex, 1)[0];
-      //   updateCartDisplay(); // aktualisiert den Warenkorb nach Entfernung
-      //   const gesamtPr = document.getElementById("gesamtp");
-      //   const alteGesamtsumme = parseFloat(gesamtPr.innerText.split(": ")[1].split("€")[0]);
-      //   const neueGesamtsumme = alteGesamtsumme - removedItem.price * removedItem.quantity;
-      //   gesamtPr.innerText = `Gesamtpreis: ${neueGesamtsumme.toFixed(2)}€`;
-      // }
-    });
+      const itemIndex = cartItems.findIndex(cartItem => cartItem.name === item.name);
+        if (itemIndex !== -1) {
+          const removedItem = cartItems.splice(itemIndex, 1)[0];
+          
+          updateCartDisplay(); // aktualisiert den Warenkorb nach Entfernung
+          
+          const gesamtPr = document.getElementById("gesamtp");
+          const alteGesamtsumme = parseFloat(gesamtPr.innerText.split(": ")[1].split("€")[0]);
+          const neueGesamtsumme = alteGesamtsumme - removedItem.price * removedItem.quantity;
+          gesamtPr.innerText = `Gesamtpreis: ${neueGesamtsumme.toFixed(2)}€`;
+          
+        }
+        
+      });
     
     
     itemDiv.appendChild(removeButton);
@@ -1039,13 +1144,11 @@ function updateCartDisplay() {
 
 }
 
-function enterfnbutton(countToRemove, preisToRemove){
-  ergebnis -= (countToRemove * preisToRemove);
-}
+
 
 function reset() {
-  // alert("Dieser Betrag wurde bezahlt.\n" + ergebnis.toFixed(2) + "€");
-
+  alert("Dieser Betrag wurde bezahlt.");
+  
   count1 = 0;count2 = 0;count3 = 0;count4 = 0;count5 = 0;count6 = 0;count7 = 0;count8 = 0;count9 = 0;count10 = 0;
   count11 = 0;count12 = 0;count13 = 0;count14 = 0;count15 = 0;count16 = 0;count17 = 0;count18 = 0;count19 = 0;count20 = 0;
   count21 = 0;count22 = 0;count23 = 0;count24 = 0;count25 = 0;count26 = 0;count27 = 0;count28 = 0;count31 = 0;count30 = 0;
